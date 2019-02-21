bd1a5f1664f8bff6222620a1b2bdd9ef5b617fc9
kostas4419 <kostas83@list.ru>
2019-02-21 14:50:37 +0300

    ДЗ первого урока

---
+++ Center/Center/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Center
+{
+    class Program
+    {
+        static void Data(string name, string lastname, string city)
+            {
+               string text = $"Имя: {name} Фамилия: {lastname} Город: {city}";
+               int length = text.Length / 2;
+
+               Console.SetCursorPosition(Console.WindowWidth / 2 - length, Console.WindowHeight / 2);
+               Console.WriteLine(text); 
+            }
+
+        static void Main()
+        {
+            /* Решение первое */
+            //string name = "Константин";
+            //string lastname = "Гордиенко";
+            //string city = "Москва";
+
+            //string text = $"Имя: {name} Фамилия: {lastname} Город: {city}";
+            //int length = text.Length/2;
+
+            //Console.SetCursorPosition(Console.WindowWidth / 2 - length, Console.WindowHeight / 2);
+            //Console.WriteLine(text);
+
+            /* решение второе */
+            Data("Константин","Гордиенко","Москва");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Swap/Swap/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swap
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите первую переменную: ");
+            var a = Console.ReadLine();
+
+            Console.Write("Введите вторую переменную: ");
+            var b = Console.ReadLine();
+
+            //var c = a;
+            //a = b;
+            //b = c;
+
+            b = b + a;
+            a = b.Substring(0,b.Length - a.Length);
+            b = b.Substring(a.Length);
+
+            Console.WriteLine("Первая переменная " + a + "\nВторая переменная "+ b + "");
+
+            Console.ReadKey();
+        }
+    }
+}
