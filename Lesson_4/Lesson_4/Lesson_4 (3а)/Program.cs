a6607cb4d18e5d6c21d90e4b582096de60e5f477
kostas4419 <kostas83@list.ru>
2019-03-04 15:48:02 +0300

    Дополнения 1.0

--- Lesson_4/Lesson_4/Lesson_4 (3а)/Program.cs
+++ Lesson_4/Lesson_4/Lesson_4 (3а)/Program.cs
@@ -9,49 +9,14 @@ namespace Lesson_4 //Гордиенко Константин, задача 3а, 
 {
     class Program
     {
-        //static public void Invers()
-        //{
-        //    int[] array2 = array.Reverse().ToArray();
-        //    foreach (int num in array2)
-        //    {
-        //        Console.Write(num + " ");
-        //    }
-        //}
-
-        //static public void Multi()
-        //{
-        //    int[] array3 = (int[])array.Clone();
-        //    // Переменная для случайных чисел
-        //    Random random = new Random();
-
-        //    // Заполняем массив случайными числами
-        //    for (int i = 0; i < array3.Length; i++)
-        //    {
-        //        array3[i] = array3[i] * random.Next(1, 9);
-        //        Console.Write(array3[i] + " ");
-        //    }
-        //}
-
-        //static public void Max()
-        //{
-        //    double max = array.Max();
-        //    double numberMax = 0;
-
-        //    for (int i = 0; i < array.Length; i++)
-        //        if (array[i] == max) numberMax++;
-
-        //    Console.WriteLine("количество максимальных элементов: {0} ", numberMax);
-        //}
+        
 
         static void Main(string[] args)
         {
+            // Передаем параметры в библиотеку (Колличество элементов, начальное значение, шаг)
            Num.NewArray(10, 1 , 5);
-            //sumArray();
-            //array.Invers();
-            //Console.WriteLine("\n");
-            //array.Multi();
-            //Console.WriteLine("\n");
-            //array.Max();
+
+            //Задержка черного экрана
             Console.ReadKey();
         }
 
