bd07513abff2b5c086b779945826077afe8ffe35
kostas4419 <kostas83@list.ru>
2019-03-05 14:54:27 +0300

    ДЗ 2, Уровень 2

---
+++ Lesson_1/Lesson_1/Game.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace MyFirstGame // Гордиенко Константин
+{
+    static class Game
+    {
+        private static BufferedGraphicsContext _context;
+        public static BufferedGraphics Buffer;
+
+        public static int Width { get; set; }
+        public static int Heigth { get; set; }
+
+        static Game()
+        {
+        }
+
+        public static void Init(Form form)
+        {
+            Graphics g;
+
+            _context = BufferedGraphicsManager.Current;
+            g = form.CreateGraphics();
+
+            Width = form.ClientSize.Width;
+            Heigth = form.ClientSize.Height;
+
+            Buffer = _context.Allocate(g, new Rectangle(0, 0, Width, Heigth));
+
+            Load();
+
+            Timer timer = new Timer();
+            timer.Interval = 20;
+            timer.Start();
+            timer.Tick += Timer_Tick;
+
+
+        }
+
+        private static void Timer_Tick(object sender, EventArgs e)
+        {
+            Draw();
+            Update();
+        }
+
+        public static void Draw()
+        {
+            Buffer.Graphics.Clear(Color.FromArgb(255, 0, 5, 15));
+
+            foreach  (BaseObject obj in _objs)
+                obj.Draw();
+            foreach (Asteroid obj in _asteroid)
+            {
+                obj.Draw();
+                _bullet.Draw();
+                Buffer.Render();
+            }
+        }
+
+        public static void Update()
+        {
+            foreach (BaseObject obj in _objs)
+                obj.Update();
+            foreach (Asteroid a in _asteroid)
+            {
+                a.Update();
+                if (a.Collision(_bullet))
+                {
+                    System.Media.SystemSounds.Hand.Play();
+                    _bullet = new Bullet(new Point(0, 200), new Point(5, 0), new Size(4, 1));
+                    var random = new Random();
+                    int r = random.Next(5, 50);
+                    _asteroid[a]. = new Asteroid(new Point(1000, random.Next(0, Game.Heigth)), new Point(-r / 5, r), new Size(r, r));
+                }
+            }
+            _bullet.Update();
+        }
+
+        private static BaseObject[] _objs;
+        private static Bullet _bullet;
+        private static Asteroid[] _asteroid;
+        public static void Load()
+        {
+            _objs = new BaseObject[30];
+            _bullet = new Bullet(new Point(0, 200), new Point(5, 0), new Size(4, 1));
+            _asteroid = new Asteroid[20];
+
+            var random = new Random();
+            for (int i = 0; i < _objs.Length; i++)
+            {
+                int r = random.Next(5, 50);
+                _objs[i] = new Star(new Point(1000, random.Next(0, Game.Heigth)), new Point(-r, r), new Size(3, 3));
+            }
+
+            
+            for (int i = 0; i < _asteroid.Length; i++)
+            {
+                int r = random.Next(5, 50);
+                _asteroid[i] = new Asteroid(new Point(1000, random.Next(0, Game.Heigth)), new Point(-r / 5, r), new Size(r, r));
+            }
+
+        }
+
+
+    }
+}
---
+++ Lesson_1/Lesson_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MyFirstGame // Гордиенко Константин
+{
+    class Program 
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            Form form = new Form
+            {
+                Width = Screen.PrimaryScreen.Bounds.Width,
+                Height = Screen.PrimaryScreen.Bounds.Height
+            };
+            Game.Init(form);
+            form.Show();
+            Game.Load();
+            Game.Draw();
+            Application.Run(form);
+        }
+    }
+} 
