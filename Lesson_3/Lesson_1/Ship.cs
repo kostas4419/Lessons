da2dae4bc4ada51632113f5b2975086948e0c336
kostas4419 <kostas83@list.ru>
2019-03-08 14:56:00 +0300

    ДЗ третьего урока

---
+++ Lesson_3/Lesson_1/Ship.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace MyFirstGame // Гордиенко Константин
+{
+    class Ship : BaseObject
+    {
+        public static event Message MessageDie;
+        private int _energy = 100;
+        public int Energy => _energy;
+
+        public void EnergyLow(int n)
+        {
+            _energy -= n;
+        }
+
+        public Ship(Point pos, Point dir, Size size) : base(pos, dir, size)
+        {
+        }
+
+        Image ship = new Bitmap("img/Ship.png");
+
+        public override void Draw()
+        {
+            Game.Buffer.Graphics.DrawImage(ship, Pos.X, Pos.Y, Size.Width, Size.Height);
+        }
+        public override void Update()
+        {
+            
+        }
+        public void Up()
+        {
+            if (Pos.Y > 0) Pos.Y = Pos.Y - Dir.Y;
+        }
+
+        public void Down()
+        {
+            if (Pos.Y < Game.Heigth) Pos.Y = Pos.Y + Dir.Y;
+        }
+
+        public void Die()
+        {
+            MessageDie?.Invoke();
+        }
+    }
+}
